ae99ad0049e96cf9662ecb93c8ed18b8b1b79e3c
Mark Gross <mark.gross2001@gmail.com>
2024-10-04 19:07:15 -0700

    fix(api): fix category tree builder
    I knew it was too simple to be correct. With the previous
    implementation, in the case of a category structure such as Income ->
    Paychecks / Side Gigs -> Photography, where Side Gigs happens to appear
    before Paychecks, the function would terminate after descending to the
    Photography category. Because the result set from the database would be
    Income (Level 0), Side Gigs (Level 1), Photography (Level 2), Paychecks
    (Level 1), after we've called GetEntryTree for Side Gigs, next is set to
    Photography, as it's the next in the list. But Photography is level 2,
    and Income is level 0, so the loop terminates, and we return Side Gigs
    as the only child of Income.
    
    By returning the tree entry as well as its index, after the call to
    GetEntryTree for Side Gigs's children, we look for the next child of
    Income at the index after the last child of Side Gigs (i.e. index 3,
    where Paychecks is). This gets us the whole tree.

--- api/Sst.Api/Features/GetCategoryTree/GetCategoryTreeQuery.cs
+++ api/Sst.Api/Features/GetCategoryTree/GetCategoryTreeQuery.cs
@@ -20,14 +20,14 @@ public partial class GetCategoryTreeQuery
     
     private record TreeEntry(string Category, List<TreeEntry> Children);
 
-    private static TreeEntry GetEntryTree(int idx, List<CategoryTreeEntry> entries)
+    private static (TreeEntry, int) GetEntryTree(int idx, List<CategoryTreeEntry> entries)
     {
         var entry = entries[idx];
         var children = new List<TreeEntry>();
         
         // end of entries; no children
         if (entries.Count == idx + 1)
-            return new TreeEntry(entry.Name, []);
+            return (new TreeEntry(entry.Name, []), -1);
         
         // get next entry
         var next = entries[++idx];
@@ -35,11 +35,15 @@ public partial class GetCategoryTreeQuery
         // add next entry's children
         while (idx != entries.Count && next.Level == entry.Level + 1)
         {
-            children.Add(GetEntryTree(idx, entries));
-            next = entries[++idx];
+            var result = GetEntryTree(idx, entries);
+            children.Add(result.Item1);
+            if (result.Item2 == -1)
+                break;
+            idx = result.Item2;
+            next = entries[idx];
         }
         
-        return new TreeEntry(entry.Name, children);
+        return (new TreeEntry(entry.Name, children), idx);
     }
 
     private static async ValueTask<CategoryTreeResponse> HandleAsync(object _, SstDbContext ctx, CancellationToken token)
@@ -47,7 +51,7 @@ public partial class GetCategoryTreeQuery
         var entries = await ctx.Set<CategoryTreeEntry>().ToListAsync(token);
         var output = entries.Select((e, i) => new { Entry = e, Index = i })
             .Where(e => e.Entry.Level == 0)
-            .Select(e => GetEntryTree(e.Index, entries))
+            .Select(e => GetEntryTree(e.Index, entries).Item1)
             .ToList();
 
         return new CategoryTreeResponse
