2c4781068aeb5a8ad8d8088c99ad01913d6cb787
Mark Gross <mark.gross2001@gmail.com>
2024-10-07 22:52:51 -0700

    fix(client): fix date display
    Months are 0-based, while days and years are 1-based. There is a special
    place in hell for whoever designed this language.

--- client/src/Timestamp.tsx
+++ client/src/Timestamp.tsx
@@ -6,7 +6,7 @@ export default function Timestamp({ ts }: TimestampProps) {
     // we have to do this apparently?
     const date = new Date(ts);
     const year = date.getFullYear();
-    const month = date.getMonth().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
 
     const hour = date.getHours().toString().padStart(2, '0');
